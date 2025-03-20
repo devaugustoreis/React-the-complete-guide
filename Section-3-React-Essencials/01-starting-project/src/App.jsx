ddb1fee112c6bcfe82c42db2d0d85dcfbd97e550
Augusto Reis <devaugustoreis@gmail.com>
2025-03-20 15:21:08 -0300

    feat: useState hook and conditional rendering

--- Section-3-React-Essencials/01-starting-project/src/App.jsx
+++ Section-3-React-Essencials/01-starting-project/src/App.jsx
@@ -1,15 +1,32 @@
-import { CORE_CONCEPTS } from "./data";
+import { useState } from 'react';
+
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
 
 function App() {
-  let tabContent = "Please click a button"
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleClick(selectedButton) {
-    tabContent = selectedButton
+    setSelectedTopic(selectedButton)
   }
+
+  /* Opção 3
+
+  let tabContent = <p>Please select a topic.</p>
+
+  if (selectedTopic) {
+    tabContent = <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+    </div>
+  }
+  */
 
   return (
     <div>
@@ -18,10 +35,10 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept 
-              title = {CORE_CONCEPTS[0].title}
-              description = {CORE_CONCEPTS[0].description}
-              image = {CORE_CONCEPTS[0].image}
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
             />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
@@ -37,7 +54,39 @@ function App() {
             <TabButton onClick={() => handleClick('props')}>Props</TabButton>
             <TabButton onClick={() => handleClick('state')}>State</TabButton>
           </menu>
+
+          {/* Opção 1 */}
+
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
+
+          {/* Opção 2 */}
+
+          {/* {!selectedTopic && <p>Please select a topic.</p>}
+          {selectedTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )} */}
+
+          {/* Opção 3 
+          
           { tabContent }
+          */}
+
         </section>
       </main>
     </div>
